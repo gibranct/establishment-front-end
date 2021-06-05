e26f36eccc6e8e2608d237c09e7d6586a1da63c4
Gibran <mac01gibran@gmail.com>
2021-06-05 16:09:28 -0300

    chore: remove lib reactotron-react-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
-import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 
 const App = () => {
